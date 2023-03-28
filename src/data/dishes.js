ad0140f789cb38f723a23fa3cb26f2b29086bd7f
Mahbub Hasan <mahbubhasan.pro@gmail.com>
2023-03-28 19:41:40 +0600

    Creating menu component

---
+++ src/data/dishes.js
@@ -0,0 +1,104 @@
+const DISHES = [
+  {
+    id: 0,
+    name: "Ginger Burger",
+    image: "assets/images/burger.jpg",
+    category: "meal",
+    label: "Hot",
+    price: "199",
+    description:
+      "A unique combination of Indian uthappam (pancake) and Italian pizza, topped with cerignola olives, ripe vine cherry tomatoes, vidalia onion, Guntur chillies and Buffalo Paneer.",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Shanto",
+        date: "2018-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Karim",
+        date: "2017-09-16T17:57:28.556094Z",
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "French Fries",
+    image: "assets/images/french.jpg",
+    category: "appetizer",
+    label: "New",
+    price: "120",
+    description:
+      "A unique combination of Indian uthappam (pancake) and Italian pizza.",
+    comments: [
+      {
+        id: 0,
+        rating: 3.5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Shanto",
+        date: "2018-10-16T17:57:28.556094Z",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    image: "assets/images/pizza.jpg",
+    category: "meal",
+    label: "Hot",
+    price: "299",
+    description:
+      "A unique combination of Indian uthappam (pancake) and Italian pizza, topped with cerignola olives, ripe vine cherry tomatoes, vidalia onion, Guntur chillies and Buffalo Paneer.",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Maruf",
+        date: "2018-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Nahid",
+        date: "2017-09-16T17:57:28.556094Z",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Coffee",
+    image: "assets/images/coffee.jpg",
+    category: "meal",
+    label: "Hot",
+    price: "99",
+    description:
+      "A unique combination of Indian uthappam (pancake) and Italian coffee.",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Mahedi",
+        date: "2018-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+        author: "Karim",
+        date: "2017-09-16T17:57:28.556094Z",
+      },
+    ],
+  },
+];
+
+export default DISHES;
