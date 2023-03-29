3b971062bdb8406ad38a0b098363d3f27607bb9f
Mahbub Hasan <mahbubhasan.pro@gmail.com>
2023-03-29 20:19:47 +0600

    Show comments

--- src/components/body/DishDetail.js
+++ src/components/body/DishDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardBody, CardImg, CardText, CardTitle } from "reactstrap";
+import LoadComments from "./LoadComments";
 
 const DishDetail = (props) => {
   return (
@@ -10,10 +11,10 @@ const DishDetail = (props) => {
           <CardTitle tag="h4" style={{ marginTop: "20px" }}>
             {props.dish.name}
           </CardTitle>
-          <CardText>
-            <p>{props.dish.description}</p>
-            <p>{props.dish.price}/-</p>
-          </CardText>
+          <CardText>{props.dish.description}</CardText>
+          <CardText>{props.dish.price}/-</CardText>
+          <hr />
+          <LoadComments comments={props.dish.comments} />
         </CardBody>
       </Card>
     </div>
---
+++ src/components/body/LoadComments.js
@@ -0,0 +1,18 @@
+import dateFormat from "dateformat";
+import React from "react";
+
+const LoadComments = (props) => {
+  return props.comments.map((comment) => {
+    return (
+      <div key={comment.id}>
+        <h5>{comment.author}</h5>
+        <p>{comment.comment}</p>
+        <p>{comment.rating}</p>
+        <p>{dateFormat(comment.date, "dddd, mmmm ds, yyyy, h:MM TT")}</p>
+        <hr />
+      </div>
+    );
+  });
+};
+
+export default LoadComments;
